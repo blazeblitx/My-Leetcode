25d356fd8418fdfd0ef87b628869a8ed0a8bda5a
blazeblitx <149155307+blazeblitx@users.noreply.github.com>
2026-09-18 10:49:45 +0530

    Time: 0 ms (100%), Space: 15 MB (25.84%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
@@ -8,14 +8,18 @@ public:
         ListNode* slow = dummy;
         ListNode* fast = dummy;
 
-        for(int i=0;i<n;i++)
+        // Move fast n + 1 steps
+        for (int i = 0; i <= n; i++) {
             fast = fast->next;
+        }
 
-        while(fast->next){
+        // Move both pointers
+        while (fast != nullptr) {
             slow = slow->next;
             fast = fast->next;
         }
 
+        // Remove the node
         slow->next = slow->next->next;
 
         return dummy->next;
